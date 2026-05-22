847083b57b174a52e979d0f777610fef6bcfdc7c
Jyoti Singh <jyotirjsingh5221@gmail.com>
2026-05-22 21:47:13 +0530

    fix: navigate import error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,Navigate } from "react-router-dom";
 import Home from"./pages/Home";
 import Login from"./pages/Login";
 import Signup from"./pages/Signup";
